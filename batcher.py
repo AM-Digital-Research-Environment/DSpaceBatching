a5a6b53a75b3001faaca0d7d45f3a79c72169248
neerajamubt <Neeraj.Thandayan-Viswajith@uni-bayreuth.de>
2024-09-25 00:02:45 +0200

    (feat): preview for archive object

--- batcher.py
+++ batcher.py
@@ -39,9 +39,14 @@ class BatchGenerator:
     def doclistbuilder(self):
         _doc_list = []
         for row in self._data:
+            _access = row.get('accessCondition').get('usage').get('type').strip().lower()
             row_dict = {
                 # Filename
                 'filename': row.get('bitstream'),
+                # DRE Identifier
+                schemamap('dreIdentifier'): row.get('dre_id'),
+                # Retention Time
+                schemamap('retention'): "publication" if _access == "public" else "storage - 15 years",
                 # Main Title
                 schemamap('title'): try_fetch(query="titleInfo[?title_type == 'main'].title[]",  document=row),
                 # Data of Issue (format "yyyy-mm-dd")
@@ -49,11 +54,13 @@ class BatchGenerator:
                 # Date of Creation (end)
                 schemamap('dateCreated'): dateconvert(try_fetch("dateInfo.created.end", document=row)),
                 # Resource Type
+                schemamap('resourceType'): row.get('typeOfResource'),
+                # General Resource Type
                 schemamap('generalResourceType'): typemap(row.get('typeOfResource')),
                 # Langauge
                 schemamap('language'): try_fetch(value=langmap(row.get('language')), direct=True),
                 # Subject Keywords
-                schemamap('subjectKeywords'): '||'.join(list(itertools.filterfalse(
+                schemamap('subject'): '||'.join(list(itertools.filterfalse(
                     lambda item: not item, list(set(try_fetch(value=[
                         try_fetch(query="genre.*[]", document=row),
                         try_fetch(query="subject[].origLabel", document=row,),
@@ -62,7 +69,7 @@ class BatchGenerator:
                 # Abstract
                 schemamap('abstract'): try_fetch(query="abstract", document=row),
                 # Technical Information
-                schemamap('technicalInformation'): '||'.join(list(itertools.filterfalse(lambda item: not item, [
+                schemamap('techInfo'): '||'.join(list(itertools.filterfalse(lambda item: not item, [
                     row.get("physicalDescription").get("type"),
                     try_fetch(query="physicalDescription.method", document=row),
                     try_fetch(query="physicalDescription.desc", document=row, delimiter=", "),
@@ -122,8 +129,9 @@ class BatchGenerator:
             """
             # Fetching unique qualifiers
             for role_type in list(set(jp.search("name[].role", row))):
-                row_dict["dc.contributor.{role_type]"] = try_fetch(
-                    query=f"name[?role == {role_type}].name.label",
+                mapped_role_type = rolemap(role_type)
+                row_dict[f"dc.contributor.{mapped_role_type}"] = try_fetch(
+                    query=f"name[?role == '{role_type}'].name.label",
                     document=row
                 )
 
@@ -157,23 +165,44 @@ class BatchGenerator:
                     ]
                 )
             )
+            # University
+            _relations_list.append(
+                " ".join(
+                    [
+                        self._relationship_types.get('university'),
+                        "02548c51-bf12-4329-88e8-659526e0b90b"
+                    ]
+                )
+            )
+            # Faculty
+            _relations_list.append(
+                " ".join(
+                    [
+                        self._relationship_types.get('faculty'),
+                        "044b2520-4890-4e13-80d1-fd9744a969c1"
+                    ]
+                )
+            )
             _relations_doc_list.append("\n".join(_relations_list))
         return _relations_doc_list
 
     # Staged values or pre-view object
     def staged_data(self):
-        return pl.DataFrame(self.doclistbuilder()).write_csv(file=None)
+        return pl.DataFrame(self.doclistbuilder()).fill_nan(None).write_csv(file=None)
 
     # Staged Related Entities
     def staged_relations(self):
         return self.relationshipsbuilder()
 
     # Create batches
-    def create_batch_dir(self):
+    def create_batch_dir(self, stage: bool = False):
         archive = DspaceArchive(
             file_folder_path=self._files_folder_path,
             metadata_object=self.staged_data(),
             relationships_object=self.relationshipsbuilder(),
-            collection_name=self._project.get('rdspace').get('collection').get('uuid')
+            collection_name=self._project.get('rdspace').get('collection').get('handle')
         )
-        archive.write(os.path.dirname(self._files_folder_path) + "\\batches")
+        if stage:
+            return archive
+        else:
+            archive.write(os.path.dirname(self._files_folder_path) + "\\batches")
