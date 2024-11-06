449acef9c98e04b936324132ead77b2e2be6af2e
neerajamubt <Neeraj.Thandayan-Viswajith@uni-bayreuth.de>
2024-11-06 11:12:32 +0100

    (mod): method added for appendind access conditions

--- safbuilder/dspacearchive.py
+++ safbuilder/dspacearchive.py
@@ -137,8 +137,9 @@ class DspaceArchive:
         contents_file = open(os.path.join(item_path, b'contents'), "wb")
 
         files = item.getFiles()
+        access_cond = item.getAccessInfo()
         for index, file_name in enumerate(files):
-            contents_file.write(file_name)
+            contents_file.write(file_name + b"\tpermissions:-r " + access_cond[index])
             if index < len(files):
                 contents_file.write(b"\n")
 
